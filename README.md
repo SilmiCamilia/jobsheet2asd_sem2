3120ba517318c23ef76c846847b8d0f7095926e6
Silmi <129129210+SilmiCamilia@users.noreply.github.com>
2026-02-22 22:16:10 +0700

    push1

---
+++ README.md
@@ -0,0 +1,10 @@
+2.1
+1. memiliki atribut, memiliki method (fungsi)
+2. atribut nama, nim, kelas, ipk
+3. tampilkanInformasi(), ubahKelas(),updateIpk(), nilaiKinerja()
+5. fungsi tersebut akan mengevaluasi ipk menggunakan kriteria rentang nilai ipk lalu mengembalikan nilai String kategori
+
+2.2.3
+1. Mahasiswa mhs1 = new Mahasiswa();
+2. memanggil nama object lalu titik (.) contoh mhs1.tampilkanInformasi();
+3. Karena isi atribut diubah menggunakan method ubahKelas() dan updateIpk()
---
+++ mahasiswa24.java
@@ -0,0 +1,40 @@
+public class mahasiswa24 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+        System.out.println("---------------------------");
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+        System.out.println("Kelas berhasil diubah menjadi " + kelas);
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+            System.out.println("IPK berhasil diperbarui.");
+        } else {
+            System.out.println("IPK tidak valid, harus antara 0.0 dan 4.0");
+        }
+    }
+
+    String nilaiKinerja(double ipk) {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.5) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
